4a10f6fa2ebea4511f3bfe4efdc57c1d296aa7f6
luv2985 <lu17vinc@gmail.com>
2023-10-15 00:38:52 -0500

    Update cpu.v wrote in most larger modules, missing wiring

--- cpu.v
+++ cpu.v
@@ -12,28 +12,55 @@
 // 
 
 module cpu (input clk, input rst_n, output hlt, output [15:0] pc);
-  // initialize instruction memory
-  // wire for instruction halfword
-  wire curr_instr;
-  // invoke fetch
-  memory1c instr (.data_out(curr_instr), .data_in(x), .addr(pc_curr), .enable, .wr(), .clk(clk), .rst(rstn));
-
-  register register_file
-  // wire for register 1 and 2 output, and opcode
-  // wire instruction to decode
-  // invoking decode, which will wire to the register file
-
-  // wire opcode to control unit
-  // wire control unit outputs to many locations, like ALU, Mem, and writeback mux
-
-  // branching is done here, wire in control unit, flag bits and make sure to sll imediate by 1
+  // initialize instruction memory how????
   
+  // wire for instruction halfword
+  wire [15:0] curr_instr;
+  // FETCH INSTRUCTION
+  memory1c instr_mem (.data_out(curr_instr), .data_in(), .addr(pc_curr), .enable, .wr(), .clk(clk), .rst(rstn));
+  
+  // control wires
+  wire regwrite;
+  wire alusrc;
+  wire memread;
+  wire memwrite;
+  wire [3:0] aluop;
+  wire memtoreg;
+  wire [1:0] branch;
+  wire regused;
+  
+  // CONTROL UNIT
+  control control_unit (.opcode(curr_instr[15:12]), .regwrite(regwrite), .alusrc(alusrc), .memread(memread), .memwrite(memwrite),
+                        .aluop(aluop), .memtoreg(memtoreg), .branch(branch), .regused(regused));
+
+  // REGISTER FILE
+  // we need to handle invalid read registers
+  // also which registers do we know to use? does it work out?
+  // we need the 0 register
+  RegisterFile register_file (.clk(clk), .rst(rstn), .SrcReg1(), .SrcReg2(), .DstReg(), .WriteReg(), .DstData(), .SrcData1(), .SrcData2());
+
+  // branch
+  //is done here, wire in control unit, flag bits and make sure to sll imediate by 1
+  pc_control pc_branch (.bsig(branch), .C(), .I(), .F(), .regsrc(), .PC_in(), .PC_out());
+
+  wire v;
+  wire alu_in1;
+  wire alu_in2;
+  wire alu_out;
   // control ALU mux for rs or imm value
-  // wire registers to ALU 
+  // ALU
+  alu alu (.aluin1(), .aluin2(), .opcode(aluop), .aluout(alu_out), .err(v))
 
-  // change flag bits
+  // FLAG
 
+  // is there no mux after alu into mem?
+  
+  wire mem_out;
   // ALU wires to memory and memory takes in 2 control unit signals, read or write
-
+  // MEM
+  memory1c mem (.data_out(mem_out), .data_in(x), .addr(pc_curr), .enable(memread), .wr(memwrite), .clk(clk), .rst(rstn));
+  
   // memory and ALU wires to mux and is controlled by cu signal, and wires to register file
+  // WRITEBACK MUX
+  
 endmodule

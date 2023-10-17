0d80104492ae4f0544e7ca8541d5fcdbb2ac4d89
Ethan Yun Ku <eyunku@gmail.com>
2023-10-16 19:49:40 -0500

    CLA fix test

--- alu.v
+++ alu.v
@@ -279,36 +279,7 @@ module red(in1, in2, out);
 endmodule
 
 /**
-* Adds two input bits, putting the result into a sum bit.
-* Takes in a carry bit.
-**/
-module full_adder(a, b, cin, s);
-    input a, b, cin;
-    output s;
-
-    // sum bit determined by a XOR b XOR cin
-    assign s = a ^ b ^ cin;
-endmodule
-
-/**
-* Generates the generate, propagate and carry out bits.
-**/
-module carry_block(a, b, cin, g, p, cout);
-    input a, b, cin;
-    output g, p, cout;
-
-    // create wires for generate and propagate bits
-    wire g, p;
-
-    assign g = a * b;
-    assign p = a + b;
-
-    // use generate and propagate bits to generate carry-out
-    assign cout = g + p * cin;
-endmodule
-
-/**
-* 4 bit carry adder. Uses 4 full adders.
+* 4 bit carry lookahead adder, using propagation and generation bits.
 * Mode of 1 means subtraction, mode of 0 means addition.
 **/
 module carry_lookahead_4bit(a, b, cin, sum, cout, mode);
@@ -319,40 +290,18 @@ module carry_lookahead_4bit(a, b, cin, sum, cout, mode);
 
     // create subtract mode
     wire[3:0] negb;
-    assign negb[0] = ~b[0];
-    assign negb[1] = ~b[1];
-    assign negb[2] = ~b[2];
-    assign negb[3] = ~b[3];
+    assign negb[3:0] = ~b[3:0];
 
-    // wires to store generate and propagate bits
-    wire p0, p1, p2, p3;
-    wire g0, g1, g2, g3;
+    // initialize propagate/generate/carry bits
+    wire[3:0] p, g, c;
+    assign p = mode ? (a ^ negb) : (a ^ b);
+    assign g = mode ? (a & negb) : (a & b);
 
-    // wire cx_y connects the carry out for bit x to the carry in for bit y
-    wire c0_1;
-    wire c1_2;
-    wire c2_3;
-    wire c3_4;
-
-    full_adder f0(.a(a[0]), .b(mode ? negb[0] : b[0]), .cin(cin), .s(sum[0]));
-    carry_block c0(.a(a[0]), .b(mode? negb[0] : b[0]), .cin(cin), .p(p0), .g(g0), .cout(c0_1));
-
-    full_adder f1(.a(a[1]), .b(mode ? negb[1] : b[1]), .cin(c0_1), .s(sum[1]));
-    carry_block c1(.a(a[1]), .b(mode ? negb[1]: b[1]), .cin(c0_1), .p(p1), .g(g1), .cout(c1_2));
-
-    full_adder f2(.a(a[2]), .b(mode ? negb[2] : b[2]), .cin(c1_2), .s(sum[2]));
-    carry_block c2(.a(a[2]), .b(mode ? negb[2]: b[2]), .cin(c1_2), .p(p2), .g(g2), .cout(c2_3));
-
-    full_adder f3(.a(a[3]), .b(mode ? negb[3] : b[3]), .cin(c2_3), .s(sum[3]));
-    carry_block c3(.a(a[3]), .b(mode ? negb[3]: b[3]), .cin(c2_3), .p(p3), .g(g3), .cout(c3_4));
-    
-    // generate carry-out of whole module
-    wire P, G;
-    
-    assign P = p0 * p1 * p2 * p3;
-    assign G = g3 + g2 * p3 + g1 * p3 * p2 + g0 * p3 * p2 * p1;
-
-    assign cout = G + P * cin;
+    assign c[0] = cin;
+    assign c[1] = g[0] | (p[0] & c[0]);
+    assign c[2]= g[1] | (p[1] & g[0]) | p[1] & p[0] & c[0];
+    assign c[3]= g[2] | (p[2] & g[1]) | p[2] & p[1] & g[0] | p[2] & p[1] & p[0] & c[0];
+    assign cout = g[3] | (p[3] & g[2]) | p[3] & p[2] & g[1] | p[3] & p[2] & p[1] & g[0] | p[3] & p[2] & p[1] & p[0] & c[0];
 endmodule
 
 /**

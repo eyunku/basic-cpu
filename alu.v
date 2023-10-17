67fa64ba97b9eecd32060f2fd8c1e118975cc3fb
Ethan Yun Ku <eyunku@gmail.com>
2023-10-16 21:47:18 -0500

    fix CLA

--- alu.v
+++ alu.v
@@ -279,7 +279,36 @@ module red(in1, in2, out);
 endmodule
 
 /**
-* 4 bit carry lookahead adder, using propagation and generation bits.
+* Adds two input bits, putting the result into a sum bit.
+* Takes in a carry bit.
+**/
+module full_adder(a, b, cin, s);
+    input a, b, cin;
+    output s;
+
+    // sum bit determined by a XOR b XOR cin
+    assign s = a ^ b ^ cin;
+endmodule
+
+/**
+* Generates the generate, propagate and carry out bits.
+**/
+module carry_block(a, b, cin, g, p, cout);
+    input a, b, cin;
+    output g, p, cout;
+
+    // create wires for generate and propagate bits
+    wire g, p;
+
+    assign g = a * b;
+    assign p = a + b;
+
+    // use generate and propagate bits to generate carry-out
+    assign cout = g + p * cin;
+endmodule
+
+/**
+* 4 bit carry adder. Uses 4 full adders.
 * Mode of 1 means subtraction, mode of 0 means addition.
 **/
 module carry_lookahead_4bit(a, b, cin, sum, cout, mode);
@@ -290,18 +319,40 @@ module carry_lookahead_4bit(a, b, cin, sum, cout, mode);
 
     // create subtract mode
     wire[3:0] negb;
-    assign negb[3:0] = ~b[3:0];
+    assign negb[0] = ~b[0];
+    assign negb[1] = ~b[1];
+    assign negb[2] = ~b[2];
+    assign negb[3] = ~b[3];
 
-    // initialize propagate/generate/carry bits
-    wire[3:0] p, g, c;
-    assign p = mode ? (a ^ negb) : (a ^ b);
-    assign g = mode ? (a & negb) : (a & b);
+    // wires to store generate and propagate bits
+    wire p0, p1, p2, p3;
+    wire g0, g1, g2, g3;
 
-    assign c[0] = cin;
-    assign c[1] = g[0] | (p[0] & c[0]);
-    assign c[2]= g[1] | (p[1] & g[0]) | p[1] & p[0] & c[0];
-    assign c[3]= g[2] | (p[2] & g[1]) | p[2] & p[1] & g[0] | p[2] & p[1] & p[0] & c[0];
-    assign cout = g[3] | (p[3] & g[2]) | p[3] & p[2] & g[1] | p[3] & p[2] & p[1] & g[0] | p[3] & p[2] & p[1] & p[0] & c[0];
+    // wire cx_y connects the carry out for bit x to the carry in for bit y
+    wire c0_1;
+    wire c1_2;
+    wire c2_3;
+    wire c3_4;
+
+    full_adder f0(.a(a[0]), .b(mode ? negb[0] : b[0]), .cin(cin), .s(sum[0]));
+    carry_block c0(.a(a[0]), .b(mode? negb[0] : b[0]), .cin(cin), .p(p0), .g(g0), .cout(c0_1));
+
+    full_adder f1(.a(a[1]), .b(mode ? negb[1] : b[1]), .cin(c0_1), .s(sum[1]));
+    carry_block c1(.a(a[1]), .b(mode ? negb[1]: b[1]), .cin(c0_1), .p(p1), .g(g1), .cout(c1_2));
+
+    full_adder f2(.a(a[2]), .b(mode ? negb[2] : b[2]), .cin(c1_2), .s(sum[2]));
+    carry_block c2(.a(a[2]), .b(mode ? negb[2]: b[2]), .cin(c1_2), .p(p2), .g(g2), .cout(c2_3));
+
+    full_adder f3(.a(a[3]), .b(mode ? negb[3] : b[3]), .cin(c2_3), .s(sum[3]));
+    carry_block c3(.a(a[3]), .b(mode ? negb[3]: b[3]), .cin(c2_3), .p(p3), .g(g3), .cout(c3_4));
+    
+    // generate carry-out of whole module
+    wire P, G;
+    
+    assign P = p0 & p1 & p2 & p3;
+    assign G = g3 | (g2 & p3) | (g1 & p3 & p2) | (g0 & p3 & p2 & p1);
+
+    assign cout = G | (P & cin);
 endmodule
 
 /**
@@ -315,25 +366,27 @@ module carry_lookahead(a, b, sum, overflow, mode);
     output[15:0] sum;
     output overflow;
 
+    wire[15:0] CLASum;
+
     // wire cx_y connects the carry out of module x to the carry in of module y
     wire c0_1;
     wire c1_2;
     wire c2_3;
 
-    carry_lookahead_4bit cla0(.a(a[3:0]), .b(b[3:0]), .cin(mode), .sum(sum[3:0]), .cout(c0_1), .mode(mode));
-    carry_lookahead_4bit cla1(.a(a[7:4]), .b(b[7:4]), .cin(c0_1), .sum(sum[7:4]), .cout(c1_2), .mode(mode));
-    carry_lookahead_4bit cla2(.a(a[11:8]), .b(b[11:8]), .cin(c1_2), .sum(sum[11:8]), .cout(c2_3), .mode(mode));
-    carry_lookahead_4bit cla3(.a(a[15:12]), .b(b[15:12]), .cin(c2_3), .sum(sum[15:12]), .cout(overflow), .mode(mode));
+    carry_lookahead_4bit cla0(.a(a[3:0]), .b(b[3:0]), .cin(mode), .sum(CLASum[3:0]), .cout(c0_1), .mode(mode));
+    carry_lookahead_4bit cla1(.a(a[7:4]), .b(b[7:4]), .cin(c0_1), .sum(CLASum[7:4]), .cout(c1_2), .mode(mode));
+    carry_lookahead_4bit cla2(.a(a[11:8]), .b(b[11:8]), .cin(c1_2), .sum(CLASum[11:8]), .cout(c2_3), .mode(mode));
+    carry_lookahead_4bit cla3(.a(a[15:12]), .b(b[15:12]), .cin(c2_3), .sum(CLASum[15:12]), .cout(overflow), .mode(mode));
 
-    // make sure arithmetic operation is saturated
-    wire[15:0] temp; // temporary sum storage
+    // // make sure arithmetic operation is saturated
+    wire[15:0] SATSum; // temporary sum storage
     wire[15:0] neg, pos; // largest negative and positive values
     assign neg = 16'h8000;
     assign pos = 16'h7fff;
 
     // if a is neg., b is neg., and output is pos.
-    assign temp = (a[15] & b[15] & ~sum[15]) ? neg : sum;
-    assign temp = (~a[15] & ~b[15] & sum[15]) ? pos : sum;
+    assign SATSum = (a[15] & b[15] & ~sum[15]) ? neg : CLASum;
+    assign SATSum = (~a[15] & ~b[15] & sum[15]) ? pos : CLASum;
 
-    assign sum = temp;
+    assign sum = SATSum;
 endmodule
--- t_alu.v
+++ t_alu.v
@@ -16,41 +16,41 @@ module t_alu();
         // test CLA
         aluop = 4'h0; aluin1 = 16'hde15; aluin2 = 16'h3f3d; // 1d52, with overflow
         #10
-        $display("Expected value: 1d52 w/ err 1. Calculated value: %h w/ err %b\n", aluout, err);
+        $display("Test ADD\nExpected value: 1d52 w/ err 1. Calculated value: %h w/ err %b\n", aluout, err);
         aluop = 4'h1; // subtraction of same values: 9ed8, no overflow
         #10
-        $display("Expected value: 9e38 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
+        $display("Test SUB\nExpected value: 9e38 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
         // test XOR
         aluop = 4'h2;
         #10
-        $display("Expected value: %h w/ err 0. Calculated value: %h w/ err %b\n", 16'hde15 ^ 16'h3f3d, aluout, err);
+        $display("Test XOR\nExpected value: %h w/ err 0. Calculated value: %h w/ err %b\n", 16'hde15 ^ 16'h3f3d, aluout, err);
         // test RED
         aluop = 4'h3; aluin1 = 16'h1122; aluin2 = 16'h9977; // 11 + 99 = aa, 22 + 77 = 99; 99 + aa = 0143
         #10
-        $display("Expected value: 0143 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
+        $display("Test RED\nExpected value: 0143 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
         // test SLL
         aluop = 4'h4; aluin1 = 16'h0001; aluin2 = 16'h0001; // shift left multiplies by 2
         #10
-        $display("Expected value: 0002 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
+        $display("Test SLL\nExpected value: 0002 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
         // test SRA
         aluop = 4'h5; aluin1 = 16'h0004; aluin2 = 16'h0001; // shift right divides by 2
         #10
-        $display("Expected value: 0002 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
+        $display("Test SRA\nExpected value: 0002 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
         // test ROR
         aluop = 4'h6; aluin1 = 16'h2222; aluin2 = 16'h0005;
         #10
-        $display("Expected value: 1111 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
+        $display("Test ROR\nExpected value: 1111 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
         // test PADDSB
         aluop = 4'h7; aluin1 = 16'h1234; aluin2 = 16'h1234;
         #10
-        $display("Expected value: 2468 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
+        $display("Test PADDSB\nExpected value: 2468 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
         // test LLB
         aluop = 4'h8; aluin1 = 16'h1111; aluin2 = 16'h8888;
         #10
-        $display("Expected value: 1188 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
+        $display("Test LLB\nExpected value: 1188 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
         // test LHB
         aluop = 4'h9;
         #10
-        $display("Expected value: 8811 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
+        $display("Test LHB\nExpected value: 8811 w/ err 0. Calculated value: %h w/ err %b\n", aluout, err);
     end
 endmodule

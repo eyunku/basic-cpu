5c29c442d844079ab16d89df6958ed041b652474
Charles Huai <charles.huai@gmail.com>
2023-11-04 01:16:04 -0500

    Fixed issue with llb, simplified code for llb + lhb

--- alu.v
+++ alu.v
@@ -67,50 +67,21 @@ module alu (aluin1, aluin2, aluop, aluout, err);
     assign err = tempErr;
 endmodule
 
-// TODO make implementation smarter
 module llb(in, imm, out);
-  input[15:0] imm;
-  input[15:0] in; 
+  input[15:0] imm; // 0xYY
+  input[15:0] in; // rd
   output[15:0] out;
 
-  assign out[15] = in[15];
-  assign out[14] = in[14];
-  assign out[13] = in[13];
-  assign out[12] = in[12];
-  assign out[11] = in[11];
-  assign out[10] = in[10];
-  assign out[9] = in[9];
-  assign out[8] = in[8];
-  assign out[7] = imm[7];
-  assign out[6] = imm[6];
-  assign out[5] = imm[5];
-  assign out[4] = imm[4];
-  assign out[3] = imm[3];
-  assign out[2] = imm[2];
-  assign out[1] = imm[2];
-  assign out[0] = imm[0];
+  assign out[15:8] = in[15:8];
+  assign out[7:0] = imm[7:0];
 endmodule
 
 module lhb(in, imm, out);
   input[15:0] in, imm;
   output[15:0] out;
-
-  assign out[15] = imm[7];
-  assign out[14] = imm[6];
-  assign out[13] = imm[5];
-  assign out[12] = imm[4];
-  assign out[11] = imm[3];
-  assign out[10] = imm[2];
-  assign out[9] = imm[1];
-  assign out[8] = imm[0];
-  assign out[7] = in[7];
-  assign out[6] = in[6];
-  assign out[5] = in[5];
-  assign out[4] = in[4];
-  assign out[3] = in[3];
-  assign out[2] = in[2];
-  assign out[1] = in[1];
-  assign out[0] = in[0];
+  
+  assign out[15:8] = imm[15:8];
+  assign out[7:0] = in[7:0];
 endmodule
 
 module sll (input [3:0] shift_amount, input [15:0] value, output [15:0] out);

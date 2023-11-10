fa8827ba9c37d43594fb42cb5d6bf80cd46141e9
Vincent Lu <lu17vinc@gmail.com>
2023-11-09 23:06:22 -0600

    updated and cleaned up pipeline register in file called pipeline_testing, testbench ccomplete for IF/ID pipeline register only, some comments on other files about how flushing and stalling work

--- hazard_detection.v
+++ hazard_detection.v
@@ -1,16 +1,23 @@
 // hazard unit
 
+// it basically flushes but also needs to freeze the pc
+// prevent PC reg and IF/ID reg from changing
 // if ID/EX.MemRead and ((ID/EX.RegisterRt = IF/ID.RegisterRs) or (ID/EX.RegisterRt = IF/ID.RegisterRt)), then stall
 
-module (dx_memread, dx_rt, fd_rs, fd_rt, stall_sig);
+module (dx_memread, fd_memread, dx_rt, fd_rs, fd_rt, stall_sig);
     // input
-    input dx_memread;
+    input dx_memread, fd_memread;
     input [3:0] dx_rt, fd_rs, fd_rt;
 
     // output
     output stall_sig;
 
     
-    assign stall_sig = (dx_memread & (dx_rt == fd_rs) | (dx_rt = fd_rt)) ? 1'b1 : 1'b0;
+    assign stall_sig = (dx_memread & ~fd_memread & (dx_rt == fd_rs) | (dx_rt = fd_rt)) ? 1'b1 : 1'b0;
+    // the ~fd_memread should prevent stalling for mem to mem case
+    // assign flush_sig = branch != 2'b0; // somehow this signal is sent from the control signal to the IF/ID registers
+
+    // include no stalling for MEM to MEM
+    // flush signal should be moved to control unit and can use the branch signal for it
 
 endmodule
--- t_hazard.v
+++ t_hazard.v
@@ -1,44 +1,37 @@
 module t_hazard ();
-  reg bsig;
-  reg clk, rst;
-  reg [2:0] flag_bits;
-  reg [15:0] SrcData1;
-  wire [15:0] instruction, pc_curr;
+  reg dx_memread, fd_memread;
+  reg [3:0] dx_rt, fd_rs, fd_rt;
+  wire stall_sig;
 
   IF dut (
         //inputs
-        .clk(clk),
-        .rst(rst),
-        .br_sig(bsig),
-        .SrcData1(SrcData1),
-        .flag_bits(flag_bits),
-        //outputs
-        .pc_out(pc_curr),
-        .instruction(instruction)
+        .dx_memread(dx_memread), 
+        .fd_memread(fd_memread), 
+        .dx_rt(dx_rt), 
+        .fd_rs(fd_rs), 
+        .fd_rt(fd_rt),
+        .stall_sig(stall_sig)
         );
 
   initial begin
+    dx_memread = 1; fd_memread = 0;
+    dx_rt = 4'h1; fd_rs = 4'h1; fd_rt = 4'h1;
     clk = 0;
     #5;
     rst = 1;
     #20;
-    bsig = 0; SrcData1 = 16'h1111; flag_bits = 3'b000; #20;
     rst = 0;
     bsig = 0; SrcData1 = 16'h1111; flag_bits = 3'b000; #20;
-    $display("pc: %b", pc_curr);
-    $display("instruction got is %b and pc %b", instruction, pc_curr);
+    $display("stall signal recieved is %b", stall_sig);
     rst = 0;
     bsig = 0; SrcData1 = 16'h1111; flag_bits = 3'b000; #20;
-    $display("pc: %b", pc_curr);
-    $display("instruction got is %b and pc %b", instruction, pc_curr);
+    $display("stall signal recieved is %b", stall_sig);
     rst = 0; 
     bsig = 0; SrcData1 = 16'h1111; flag_bits = 3'b000; #20;
-    $display("pc: %b", pc_curr);
-    $display("instruction got is %b and pc %b", instruction, pc_curr);
+    $display("stall signal recieved is %b", stall_sig);
     rst = 0; 
     bsig = 0; SrcData1 = 16'h1111; flag_bits = 3'b000; #20;
-    $display("pc: %b", pc_curr);
-    $display("instruction got is %b and pc %b", instruction, pc_curr);
+    $display("stall signal recieved is %b", stall_sig);
     $stop;
   end
   

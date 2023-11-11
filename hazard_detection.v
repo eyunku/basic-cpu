b930ba417d3460a28d594d890ebd9561049a78f0
Vincent Lu <lu17vinc@gmail.com>
2023-11-10 22:53:27 -0600

    pipeline registers debugged, included reg rs detection to hazard detection unit

--- hazard_detection.v
+++ hazard_detection.v
@@ -4,20 +4,33 @@
 // prevent PC reg and IF/ID reg from changing
 // if ID/EX.MemRead and ((ID/EX.RegisterRt = IF/ID.RegisterRs) or (ID/EX.RegisterRt = IF/ID.RegisterRt)), then stall
 
-module (dx_memread, fd_memread, dx_rt, fd_rs, fd_rt, stall_sig);
+/*
+It is highly recommended to make a table that lists (for each instruction) the control signals needed at each pipeline
+stage. Also, make a table listing the input signals and output signals for the data and control hazard detection units
+(data hazard stalls and control hazard flushes). The data hazard detection unit should be aware of available data
+forwarding and register bypassing
+*/
+
+// TODO: include stalling for branch rs  (2 stalls), may have to handelled elsewhere
+
+module hazard_unit (dx_memread, fd_memread, dx_rt, fd_rs, fd_rt, stall_sig, rs_dep);
     // input
     input dx_memread, fd_memread;
     input [3:0] dx_rt, fd_rs, fd_rt;
 
     // output
     output stall_sig;
+    output rs_dep;
 
+
+
+    assign rs_dep = (dx_memread & (dx_rt == fd_rs)) ? 1'b1 : 1'b0; // may be used to find branchrs dependency
     
-    assign stall_sig = (dx_memread & ~fd_memread & (dx_rt == fd_rs) | (dx_rt = fd_rt)) ? 1'b1 : 1'b0;
-    // the ~fd_memread should prevent stalling for mem to mem case
+    // the ~fd_memread should prevent stalling for mem to mem case, also note that fd suffix refers to wires from the curr run of decode
     // assign flush_sig = branch != 2'b0; // somehow this signal is sent from the control signal to the IF/ID registers
+    assign stall_sig = (dx_memread & ~fd_memread & ((dx_rt == fd_rs) | (dx_rt == fd_rt))) ? 1'b1 : 1'b0;
 
-    // include no stalling for MEM to MEM
-    // flush signal should be moved to control unit and can use the branch signal for it
 
 endmodule
+
+// for the branch rs case we require the stall signal to come from the pc
--- t_hazard.v
+++ t_hazard.v
@@ -1,37 +1,47 @@
 module t_hazard ();
+  reg clk, rst;
   reg dx_memread, fd_memread;
   reg [3:0] dx_rt, fd_rs, fd_rt;
-  wire stall_sig;
+  wire stall_sig, rs_dep;
 
-  IF dut (
+  hazard_unit dut (
         //inputs
         .dx_memread(dx_memread), 
         .fd_memread(fd_memread), 
         .dx_rt(dx_rt), 
         .fd_rs(fd_rs), 
         .fd_rt(fd_rt),
-        .stall_sig(stall_sig)
+        .stall_sig(stall_sig),
+        .rs_dep(rs_dep)
         );
 
   initial begin
-    dx_memread = 1; fd_memread = 0;
     dx_rt = 4'h1; fd_rs = 4'h1; fd_rt = 4'h1;
     clk = 0;
     #5;
     rst = 1;
     #20;
     rst = 0;
-    bsig = 0; SrcData1 = 16'h1111; flag_bits = 3'b000; #20;
-    $display("stall signal recieved is %b", stall_sig);
+    dx_memread = 1; fd_memread = 1;
+    dx_rt = 4'h1; fd_rs = 4'h1; fd_rt = 4'h1; #20;
+    $display("stall signal recieved is %b and %b", stall_sig, rs_dep);
     rst = 0;
-    bsig = 0; SrcData1 = 16'h1111; flag_bits = 3'b000; #20;
-    $display("stall signal recieved is %b", stall_sig);
+    dx_rt = 4'h1; fd_rs = 4'h1; fd_rt = 4'h1; #20;
+    $display("stall signal recieved is %b and %b", stall_sig, rs_dep);
     rst = 0; 
-    bsig = 0; SrcData1 = 16'h1111; flag_bits = 3'b000; #20;
-    $display("stall signal recieved is %b", stall_sig);
+    dx_memread = 0; fd_memread = 0;
+    dx_rt = 4'h1; fd_rs = 4'h1; fd_rt = 4'h1; #20;
+    $display("stall signal recieved is %b and %b", stall_sig, rs_dep);
     rst = 0; 
-    bsig = 0; SrcData1 = 16'h1111; flag_bits = 3'b000; #20;
-    $display("stall signal recieved is %b", stall_sig);
+    dx_memread = 1; fd_memread = 0;
+    dx_rt = 4'h1; fd_rs = 4'h1; fd_rt = 4'h1; #20;
+    $display("stall signal recieved is %b and %b", stall_sig, rs_dep);
+    dx_memread = 0; fd_memread = 1;
+    dx_rt = 4'h1; fd_rs = 4'h1; fd_rt = 4'h1; #20;
+    $display("stall signal recieved is %b and %b", stall_sig, rs_dep);
+    dx_memread = 1; fd_memread = 1;
+    dx_rt = 4'h1; fd_rs = 4'h1; fd_rt = 4'h1; #20;
+    $display("stall signal recieved is %b and %b", stall_sig, rs_dep);
     $stop;
   end
   

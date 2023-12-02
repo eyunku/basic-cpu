9dc0bf45d1508bbdcd23392f49b1d8508d21a9fc
luv2985 <lu17vinc@gmail.com>
2023-12-02 07:24:11 -0600

    Update metadata_array.v so we hav 64 sets instead of 128

--- Cache Processor/metadata_array.v
+++ Cache Processor/metadata_array.v
@@ -4,7 +4,7 @@
 //WriteEnable is one on writes and zero on reads
 
 module MetaDataArray(input clk, input rst, input [7:0] DataIn, input Write, input [127:0] BlockEnable, output [7:0] DataOut);
-	MBlock Mblk[127:0]( .clk(clk), .rst(rst), .Din(DataIn), .WriteEnable(Write), .Enable(BlockEnable), .Dout(DataOut));
+	MBlock Mblk[64:0](.clk(clk), .rst(rst), .Din(DataIn), .WriteEnable(Write), .Enable(BlockEnable), .Dout(DataOut));
 endmodule
 
 module MBlock( input clk,  input rst, input [7:0] Din, input WriteEnable, input Enable, output [7:0] Dout);
@@ -16,4 +16,3 @@ module MCell( input clk,  input rst, input Din, input WriteEnable, input Enable,
 	assign Dout = (Enable & ~WriteEnable) ? q:'bz;
 	dff dffm(.q(q), .d(Din), .wen(Enable & WriteEnable), .clk(clk), .rst(rst));
 endmodule
-

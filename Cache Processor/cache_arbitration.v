340302a224765ce58113113b8f79a8f4ddaafdeb
millewave <40739837+millewave@users.noreply.github.com>
2023-12-10 16:15:35 -0600

    Memory arbitration (#48)
    * Init of cache to mem interface
    
    * Init version of cache to mem interface w/ simple tb to check for unexpected output
    
    * Implemented simple tb on arbitration module
    
    * Completed tb for cache_arbitration.v
    
    * Reshuffled files around + init integration test for cache controller and memory arbitration
    
    * Basic check to see if any issues occured during arbitration + cache controller integration
    
    * Added 2 case checks
    
    * Fixed issue with combinational loop produced during integration, need more tests
    
    * working version for write integration in tb
    
    * Finished case 3
    
    * Removed executable file
    
    * Cleaned up tb arbitration module
    
    * Typo

---
+++ Cache Processor/cache_arbitration.v
@@ -0,0 +1,71 @@
+// d_read will be enabled in two scenarios
+// cache controller, when a read miss occurs
+// cache, when a write is being donw
+
+// i_read will only be enabled when a read miss occurs
+
+// d_addr will be determined based on two scenarios
+// cache controller, determines incoming address to read from memory
+// cache, determines address to write to memory
+
+// d_data will be sent when cache wants to write to memory
+
+// i_addr is the addr that cache_i wants to read from memory
+
+// output is determined by cache_to_mem
+// depending on which cache requested memory, cache_to_mem will send
+// the information through the corresponding output and asser the proper valid signals
+
+// NOTE: features not incoporated into the interface yet (may have to handle this outside)
+// - On either a read miss or write for cache_d, the proper d_addr must be provided (MUX required)
+// - Cache will have to determine when to stop requesting writes
+
+module cache_to_mem(
+    input clk, rst,
+    input d_enable, d_write, i_enable,
+    input [15:0] d_addr, d_data, i_addr,
+    output d_valid, i_valid,
+    output [15:0] data_out
+);
+    parameter CACHE_D = 2'h2;
+    parameter CACHE_I = 2'h1;
+    parameter CACHE_NONE = 2'h0;
+
+    wire enable, wr;
+    wire [1:0] who;
+    wire [15:0] addr;
+    // Determine which cache controller receives data_out
+    // prioritize cache_d requests
+    assign who = (d_enable | d_enable & d_write) ? CACHE_D :
+                 (i_enable) ? CACHE_I : CACHE_NONE;
+    
+    wire [1:0] who_next;
+    dff WHO[1:0] (.q(who_next[1:0]), .d(who[1:0]), .wen(1'b1), .clk(clk), .rst(rst));
+
+    // set memory input based on cache
+    assign enable = (who == CACHE_D) ? d_enable : 
+                    (who == CACHE_I) ? i_enable : 1'b0;
+    assign wr = (who == CACHE_D) ? d_write : 1'b0;
+    assign addr = (who == CACHE_D) ? d_addr : i_addr;
+
+    /** 
+    * multi-cycle module here 
+    **/
+    // note: 
+    // d_data is already handled by wr
+    // d_out can be sent to both caches, just need to ensure that proper valid bits are set
+    wire data_valid;
+    memory4c main_mem (
+        .clk(clk), 
+        .rst(rst),
+        .enable(enable),
+        .wr(wr), 
+        .data_in(d_data), 
+        .addr(addr),  
+        .data_valid(data_valid), 
+        .data_out(data_out)
+    );
+
+    assign d_valid = (who_next == CACHE_D) ? data_valid : 1'b0;
+    assign i_valid = (who_next == CACHE_I) ? data_valid : 1'b0;
+endmodule
---
+++ Cache Processor/testbenches/t_cache_arbitration.v
@@ -0,0 +1,111 @@
+`include "multicycle_memory.v"
+`include "cache_arbitration.v"
+
+module t_arbitration();
+    reg clk, rst;
+    reg d_enable, d_write, i_enable;
+    reg [15:0] d_addr, d_data, i_addr;
+    wire d_valid, i_valid;
+    wire [15:0] data_out;
+
+    cache_to_mem dut (
+        .clk(clk), .rst(rst), 
+        .d_enable(d_enable), .d_write(d_write), .i_enable(i_enable), 
+        .d_addr(d_addr), .d_data(d_data), .i_addr(i_addr),
+        .d_valid(d_valid), .i_valid(i_valid),
+        .data_out(data_out)
+    );
+
+    /**
+    * Scenario Considerations
+    * - Incoming request from one of the caches
+    * - Incoming request from both of the caches (How to handle)
+    * - Read request action
+    * - Write request action
+    **/
+
+    // Case 1: no action
+
+    // Case 2: write request
+
+    // Case 2a: read request from either cache-I or cache-D
+
+    // Case 2b: two read requests from either cache
+
+    // Case 3a: write request from cache-D
+
+    // Case 3b: read request from cache-I and write request from cache-D
+
+    initial begin
+        clk = 1'b0; rst = 1'b1;
+        #80
+        rst = 1'b0;
+        #20
+        // Case 1: no action taken
+        d_enable = 1'b0; d_write = 1'b0; i_enable = 1'b0; 
+        d_addr = 16'h0; d_data = 16'h0; i_addr = 16'h0;
+        #20
+        if (d_valid == 1'b1 | i_valid == 1'b1) begin
+            $display("Case 1 Error: no action expected");
+            $display("d_valid: %b i_valid: %b data_out: %h", d_valid, i_valid, data_out);
+        end
+
+        // Case 2: write + read block
+        d_enable = 1'b1; d_write = 1'b1; i_enable = 1'b0; 
+        d_addr = 16'h0; d_data = 16'hFFFF; i_addr = 16'h0;
+        #20
+        // should not recieve any output after write
+        if (d_valid == 1'b1 | i_valid == 1'b1) begin
+            $display("Case 2 Error: no output after write expected");
+            $display("d_valid: %b i_valid: %b data_out: %h", d_valid, i_valid, data_out);
+        end
+
+        // Read request by cache-D
+        d_enable = 1'b1; d_write = 1'b0; i_enable = 1'b0; 
+        d_addr = 16'h0; d_data = 16'h0; i_addr = 16'h0;
+        #80
+        if (d_valid == 1'b0 | i_valid == 1'b1 | data_out != 16'hFFFF) begin
+            $display("Case 2 Error: output after read expected");
+            $display("d_valid: %b i_valid: %b data_out: %h", d_valid, i_valid, data_out);
+        end
+
+        // Case 3: read request from cache-I and write request from cache-D in parallel
+        d_enable = 1'b1; d_write = 1'b1; i_enable = 1'b1; 
+        d_addr = 16'h2; d_data = 16'h1234; i_addr = 16'h2;
+        #20
+        // Write request is deasserted, read request is maintained
+        d_enable = 1'b0; d_write = 1'b0; i_enable = 1'b1; 
+        d_addr = 16'h2; d_data = 16'h1234; i_addr = 16'h2;
+        #60
+        // 4 cycles since write request and 3 cycles since read request
+        // Check that read was not prioritized over a write
+        if (d_valid == 1'b1 | i_valid == 1'b1) begin
+            $display("Case 3 Error: cache-I read is prioritized over cache-D write");
+            $display("d_valid: %b i_valid: %b data_out: %h", d_valid, i_valid, data_out);
+        end
+        #20
+        // Check for read request
+        if (d_valid == 1'b1 | i_valid == 1'b0 | data_out != 16'h1234) begin
+            $display("Case 3 Error: read request after write expected");
+            $display("d_valid: %b i_valid: %b data_out: %h", d_valid, i_valid, data_out);
+        end
+
+        // Case 4: check incoming read requests from both caches
+        d_enable = 1'b1; d_write = 1'b0; i_enable = 1'b1; 
+        d_addr = 16'h0; d_data = 16'hFFFF; i_addr = 16'h2;
+        #80
+        // Check that cache-D was requested first
+        if (d_valid == 1'b0 | i_valid == 1'b1 | data_out != 16'hFFFF) begin
+            $display("Case 4 Error: cache-D read request expected");
+            $display("d_valid: %b i_valid: %b data_out: %h", d_valid, i_valid, data_out);
+        end
+
+    
+        $stop;
+    end
+
+    always begin
+        #10;
+        clk = ~clk;
+    end
+endmodule
